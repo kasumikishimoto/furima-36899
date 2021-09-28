6545f2cff285b244a8aee288decfd14210640bf2
kasumikishimoto <kasumikishimot00@gmail.com>
2021-09-28 16:53:26 +0900

    usersテーブル バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  validates :email,           presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/ }
+  validates :password,        presence: true, format: { with: /[a-z\d]{6,}/i }
+  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday,        presence: true
+
 end
