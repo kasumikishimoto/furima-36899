6f0e2956aa3aef157f6ac00a97b186395c9dd3a6
kasumikishimoto <kasumikishimot00@gmail.com>
2021-09-29 15:16:56 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
   validates :email,           presence: true, uniqueness: true
   validates :password,        presence: true, format: { with: /[a-z\d]{6,}/i }
   validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+  validates :first_name_kana
+  validates :last_name_kana
+  end
   validates :birthday,        presence: true
 
   has_many :items
