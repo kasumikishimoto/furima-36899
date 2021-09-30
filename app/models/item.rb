6dfc8d5a3ce1a62ab66be660dcd8fdf6b24f1008
kasumikishimoto <kasumikishimot00@gmail.com>
2021-09-30 12:25:48 +0900

    item バリデーションの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,20 @@
 class Item < ApplicationRecord
 
   validates :title,           presence: true
-  validates :category_id,     presence: true
+  validates :category_id,     numericality: { other_than: 1 }
   validates :description,     presence: true
-  validates :status_id,       presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id,   presence: true
-  validates :day_to_ship_id,  presence: true
+  validates :status_id,       numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id,   numericality: { other_than: 1 }
+  validates :day_to_ship_id,  numericality: { other_than: 1 }
   validates :price,           presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :day_to_ship
 
   belongs_to :user
   has_one_attached :image
