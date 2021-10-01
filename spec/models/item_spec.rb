fe562f11f01142e094ce059de18c7bc8b3d4b52f
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-01 14:59:14 +0900

    テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.build(:user)
+    @item = FactoryBot.build(:item)
   end
 
   describe '商品出品' do
@@ -14,46 +14,57 @@ RSpec.describe Item, type: :model do
         @item.title = 'あいうえお'
         expect(@item).to be_valid
       end
-      it 'descriptionが1000文字以内であれば出品できる'
+      it 'descriptionが1000文字以内であれば出品できる' do
         @item.description = 'ああああああああああああ'
         expect(@item).to be_valid
+      end
     end
 
     context '出品できないとき' do
-      it 'titleのidが1では出品できない' do
-        @item.title_id = 1
+      it 'titleのidが空では出品できない' do
+        @item.title = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include()
+        expect(@item.errors.full_messages).to include("Title can't be blank")
       end
-      it 'descriptionのidが1では出品できない' do
-        @item.describe_id = 1
+      it 'imageが空では登録できない' do
+        @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include()
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank", "Price is not included in the list", "Price is invalid")
+      end
+      it 'descriptionが空では出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'categoryのidが1では出品できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include()
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it 'statusのidが1では出品できない' do
         @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include()
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it 'shipping_feeのidが1では出品できない' do
         @item.shipping_fee_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Shipping fee must be other than 1")
       end
       it 'prefectureのidが1では出品できない' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include()
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it 'day_to_shipのidが1では出品できない' do
         @item.day_to_ship_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include()
+        expect(@item.errors.full_messages).to include("Day to ship must be other than 1")
       end
     end
   end
