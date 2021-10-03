f8bf919da9b5eb4c6671ac1a2d99f1205bcc472a
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-03 12:49:21 +0900

    ルーティングとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item =  Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:title, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
