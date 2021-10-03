4cddccbedebad3d4b0ec5d5048c7ff15d712e82b
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-03 13:07:10 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item =  Item.find(params[:id])
+    item =  Item.find(params[:id])
+    binding.pry
+    if item.destroy
+      redirect_to root_path
+    end
   end
 
   private
