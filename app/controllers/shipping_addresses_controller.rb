b382646cbe106f8d758cb7ad5d5a317a5c3a4a4c
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-05 22:45:51 +0900

    購入画面の表示と購入機能

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -2,21 +2,24 @@ class ShippingAddressesController < ApplicationController
 
   def index
     @form = Form.new
-  end
-
-  def new
-    
+    @item = Item.find(params[:item_id])
   end
 
 
   def create
-    binding.pry
-    @form = Form.save
+    @form = Form.new(shipping_params)
+    if @form.valid?
+      @form.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
 
-  def private
-    params.require(:shipping_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_nu, :purchase_information).merge(user_id: current_user.id, item_id: params[:item_id])
+  private
+  def shipping_params
+    params.require(:form).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_nu, :purchase_information, :image).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
