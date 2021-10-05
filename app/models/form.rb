d5c1bf72cb5873a2d2ec285a94316fe181ea36a6
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-05 22:46:55 +0900

    formオブジェクト修正

--- app/models/form.rb
+++ app/models/form.rb
@@ -15,8 +15,8 @@ class Form
   
 
   def save
-    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, phone_nu: phone_nu, purchase_information_id: purchase_information.id)
     purchase_information = PurchaseInformation.create(item_id: item_id, user_id: user_id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, phone_nu: phone_nu, purchase_information_id: purchase_information.id)
   end
 
 end
