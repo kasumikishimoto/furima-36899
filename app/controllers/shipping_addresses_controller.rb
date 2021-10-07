efd714a70519cfd9f4cc4180c3a7c2c613cd1abd
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-07 23:16:24 +0900

    ユーザー・商品状態ごとの条件分岐

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -1,13 +1,15 @@
 class ShippingAddressesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :create_instance
+  before_action :move_to_index
+  before_action :same_user
 
   def index
     @form = Form.new
-    @item = Item.find(params[:item_id])
   end
 
 
   def create
-    @item = Item.find(params[:item_id])
     @form = Form.new(shipping_params)
     if @form.valid?
       pay_item
@@ -23,7 +25,23 @@ class ShippingAddressesController < ApplicationController
   def shipping_params
     params.require(:form).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_nu).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
-  
+
+  def create_instance
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id && @item.purchase_information != nil
+      redirect_to root_path
+    end
+  end
+
+  def same_user
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
   def  pay_item
     Payjp.api_key = ENV["FURIMA_SECRET_KEY"]
     Payjp::Charge.create(
@@ -33,3 +51,5 @@ class ShippingAddressesController < ApplicationController
     )
   end
 end
+
+#トップページに戻す　もし　（今ログインしているユーザー　＝＝　出品者）　　商品の購入の履歴があるかどうか
