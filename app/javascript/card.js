8d0b894b2a1dddae957843aa68cc7586f921bc89
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-07 17:09:19 +0900

    jsファイルの編集

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_d3f41b6cdb95c6bce67d6c85");
+  Payjp.setPublicKey(process.env.FURIMA_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
@@ -13,15 +13,22 @@ const pay = () => {
       exp_year: `20${formData.get("form[exp_year]")}`,
       cvc: formData.get("form[cvc]")
     };
-    console.log(card)
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value = ${token} name = 'token' type = "hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
-
 window.addEventListener("load", pay);
