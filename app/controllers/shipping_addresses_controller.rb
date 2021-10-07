4ae49178e90fa7245875cdd65eaf3f3756f58c05
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-07 17:09:38 +0900

    コントローラーの修正

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -10,6 +10,7 @@ class ShippingAddressesController < ApplicationController
     @item = Item.find(params[:item_id])
     @form = Form.new(shipping_params)
     if @form.valid?
+      pay_item
       @form.save
       redirect_to root_path
     else
@@ -20,6 +21,15 @@ class ShippingAddressesController < ApplicationController
 
   private
   def shipping_params
-    params.require(:form).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_nu).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:form).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_nu).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+  
+  def  pay_item
+    Payjp.api_key = ENV["FURIMA_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: shipping_params[:token],
+      currency: 'jpy'
+    )
   end
 end
